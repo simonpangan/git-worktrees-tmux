9c831ecc562832d9b91b5344005addac61623e6e
Simon Pangan <simonjoseph.pangan@gmail.com>
2025-10-08 23:46:42 +0800

    add worktree initial commands

---
+++ RemoveWorktree.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RemoveWorktree extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'worktree:remove';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
